559627556419e8857a2adfc9d17702be237a28f6
Tovborg <emiltovborg11@gmail.com>
2022-09-04 15:49:38 +0200

    testing

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# git
 import os
 from pathlib import Path
 # import get random secret key
