74e7a6f54ebd8e28de867354f8ab14042695be63
Tovborg <emiltovborg11@gmail.com>
2022-09-05 17:42:21 +0200

    bug fix

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -4,16 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 # import lectio.views so that it also works in production
-from .views import *
 
+
+def home(request):
+    return JsonResponse({'message': 'Hello, Django!'})
 
 urlpatterns = [
-    path('api/first_lecture_status/', check_first_lecture_status,
-         name='first_lecture_status'),
-    path('api/get_schedule/', get_schedule, name='get_schedule'),
-    path('api/get_todays_schedule/', get_todays_schedule,
-         name='get_todays_schedule'),
-    path('admin/', admin.site.urls),
+    # path('api/first_lecture_status/', check_first_lecture_status,
+    #      name='first_lecture_status'),
+    # path('api/get_schedule/', get_schedule, name='get_schedule'),
+    # path('api/get_todays_schedule/', get_todays_schedule,
+    #      name='get_todays_schedule'),
+    # path('admin/', admin.site.urls),
     path('', home, name='home'),
 ]
 
