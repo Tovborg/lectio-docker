920c3755b98cd16423999eae57e3abc3faeb702b
Tovborg <emiltovborg11@gmail.com>
2022-09-05 17:45:19 +0200

    forgot to do pip freeze

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -4,18 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 # import lectio.views so that it also works in production
-
-
-def home(request):
-    return JsonResponse({'message': 'Hello, Django!'})
+from lectio.views import *
 
 urlpatterns = [
-    # path('api/first_lecture_status/', check_first_lecture_status,
-    #      name='first_lecture_status'),
-    # path('api/get_schedule/', get_schedule, name='get_schedule'),
-    # path('api/get_todays_schedule/', get_todays_schedule,
-    #      name='get_todays_schedule'),
-    # path('admin/', admin.site.urls),
+    path('api/first_lecture_status/', check_first_lecture_status,
+         name='first_lecture_status'),
+    path('api/get_schedule/', get_schedule, name='get_schedule'),
+    path('api/get_todays_schedule/', get_todays_schedule,
+         name='get_todays_schedule'),
+    path('admin/', admin.site.urls),
     path('', home, name='home'),
 ]
 
