aa7271a89cd4ec31c3ee7bed1be739aa2bb5ce1b
Tovborg <emiltovborg11@gmail.com>
2022-09-05 16:44:32 +0200

    testing

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -3,12 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
-from .views import home
+from lectio.views import *
 
 
 urlpatterns = [
-    path('', include('lectio.urls')),
+    path('api/first_lecture_status/', check_first_lecture_status,
+         name='first_lecture_status'),
+    path('api/get_schedule/', get_schedule, name='get_schedule'),
+    path('api/get_todays_schedule/', get_todays_schedule,
+         name='get_todays_schedule'),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
 
 if bool(settings.DEBUG):
