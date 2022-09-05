847e9c6527f000449daf9a7d82ff3fb36057b1a9
Tovborg <emiltovborg11@gmail.com>
2022-09-05 17:53:54 +0200

    moved from django project to django app

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -7,13 +7,8 @@ from django.http import JsonResponse
 from lectio.views import *
 
 urlpatterns = [
-    path('api/first_lecture_status/', check_first_lecture_status,
-         name='first_lecture_status'),
-    path('api/get_schedule/', get_schedule, name='get_schedule'),
-    path('api/get_todays_schedule/', get_todays_schedule,
-         name='get_todays_schedule'),
+    path('', include('lectio.urls')),
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
 ]
 
 if bool(settings.DEBUG):
--- app/lectio/urls.py
+++ app/lectio/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import *
+from lectio.views import *
 
 namespace = 'lectio'
 
 urlpatterns = [
+    path('api/first_lecture_status/', check_first_lecture_status,
+         name='first_lecture_status'),
+    path('api/get_schedule/', get_schedule, name='get_schedule'),
+    path('api/get_todays_schedule/', get_todays_schedule,
+         name='get_todays_schedule'),
     path('', home, name='home'),
 ]
