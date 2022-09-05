28d701fc57adb9502fcae3b06b47b91380aebcd2
Tovborg <emiltovborg11@gmail.com>
2022-09-05 15:59:43 +0200

    configured views.py to be correct

--- app/lectio/urls.py
+++ app/lectio/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from lectio.views import *
+
 namespace = 'lectio'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('api/first_lecture_status/', check_first_lecture_status,
+         name='first_lecture_status'),
+    path('api/get_schedule/', get_schedule, name='get_schedule'),
+    path('api/get_todays_schedule/', get_todays_schedule,
+         name='get_todays_schedule'),
 ]
